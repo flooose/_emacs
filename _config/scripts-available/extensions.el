d18a7ddbd7c663f850ef39d2876b85b338e4151f
christopher floess <skeptikos@gmail.com>
2013-11-24 09:25:06 +0100

    refactor incrementing functionality

--- _config/scripts-available/extensions.el
+++ _config/scripts-available/extensions.el
@@ -1,25 +1,28 @@
-(defun increment-next-number ()
-  "increments the next integer in the current line"
-  (interactive)
+(defun search-and-calculate-for (fun)
+  "searches for occurence of next integer and performs given action on it"
   (save-excursion
     (search-forward-regexp "[[:digit:]]" (point-at-eol))
     (let ((number (char-before)))
       (deactivate-mark)
       (delete-backward-char 1)
-      (insert (char-to-string (+ 1 number)))))
+      (funcall fun number))
+    ))
 
-    )
+(defun increment-next-number ()
+  "increments the next integer in the current line"
+  (interactive)
+  (search-and-calculate-for
+   (lambda (number)
+     (insert-char (+ number 1)))
+   ))
 
 (defun decrement-next-number ()
   "decrements the next integer in the current line"
   (interactive)
-  (save-excursion
-    (search-forward-regexp "[[:digit:]]" (point-at-eol))
-    (let ((number (char-before)))
-      (deactivate-mark)
-      (delete-backward-char 1)
-      (insert-char (- number 1)))
-    ))
+  (search-and-calculate-for
+   (lambda (number)
+     (insert-char (- number 1)))
+   ))
 
 (global-set-key (kbd "C-+") 'increment-next-number)
 (global-set-key (kbd "C-M-+") 'decrement-next-number)
