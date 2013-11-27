b758218a18354ce57a5e703d88af10fd572bacc9
christopher floess <skeptikos@gmail.com>
2013-11-27 07:53:32 +0100

    add marmalade to package archives

--- init.el
+++ init.el
@@ -12,6 +12,8 @@
 (setq package-user-dir "~/.emacs.d/elpa/")
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 ;; outsource customization
