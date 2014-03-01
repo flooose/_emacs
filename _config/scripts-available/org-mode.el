198dc60071bc41dae6375d73d4b6c95a35b10b4d
christopher floess <skeptikos@gmail.com>
2014-03-01 10:53:29 +0100

    Separate org-mobile-directory from org-directory
    As well as other changes
    
    starting # with '#' will be ignored, and an empty message aborts the
    commit.  # On branch master # Your branch is ahead of 'origin/master'
    by 1 commit.  # (use "git push" to publish your local commits) # #
    Changes to be committed: # modified:
    _config/scripts-available/org-mode.el # modified: emacs-custom.el # #
    Changes not staged for commit: # modified: init.el # # Untracked
    files: # .org-id-locations # bookmarks #

--- _config/scripts-available/org-mode.el
+++ _config/scripts-available/org-mode.el
@@ -1,17 +1,31 @@
-(setq org-directory "/scpc:chris@192.168.1.200:public_html/org")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(setq org-directory "~/Spaces/org-mode")
+(setq org-default-notes-file (concat org-directory "/index.org"))
 (define-key global-map "\C-cc" 'org-capture)
 (setq org-capture-templates
-     '(("t" "Todo" entry (file+headline headline "/scpc:chris@192.168.1.200:public_html/org/notes.org" "Tasks")
+     '(("t" "Todo" entry (file+headline headline (concat org-directory "/index.org") "Tasks")
              "* TODO %?\n  %i\n  %a")
-       ("j" "Journal" entry (file+datetree "/scpc:chris@192.168.1.200:public_html/org/journal.org")
+       ("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
       "* %?\nEntered on %U\n  %i\n  %a")))
 
-;(setq org-agenda-files "/scpc:chris@192.168.1.200:public_html/org/") ;; when a directory, all org files are agenda files
-;;; org-mobile-files defaults to all agenda files
+(setq org-agenda-files (list org-directory))
+
+;; mobileorg
+(setq org-mobile-directory "/scpc:chris@192.168.1.200:public_html/org/")
+(setq org-mobile-keywords (list "shopping" "todo"))
+(setq org-mobile-inbox-for-pull (concat org-directory "/from-mobile.org"))
+(setq org-mobile-files (list
+                   (concat org-directory "/index.org")
+                   (concat org-directory "/todo.org")
+                   (concat org-directory "/shopping.org")
+                   (concat org-directory "/vokab.org")))
+
 ;;; org-todo-keywords has sane default
 ;;; org-tag-alist still has to be set
-;(setq org-mobile-directory "/scpc:chris@192.168.1.200:public_html/org/")
 ;
 
 
--- emacs-custom.el
+++ emacs-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function (quote browse-url-chromium))
  '(menu-bar-mode nil)
- '(org-agenda-files (quote (#("/scpc:chris@192.168.1.200:/home/chris/public_html/org/journal.org" 54 65 (org-attr nil)) #("/scpc:chris@192.168.1.200:/home/chris/public_html/org/mobileorg.org" 54 67 (org-attr nil)) "/scpc:chris@192.168.1.200:/home/chris/public_html/org/index.org")))
+ '(org-agenda-files (quote ("/scp:192.168.1.200:/home/chris/public_html/org/mobileorg.org" #("/scpc:chris@192.168.1.200:/home/chris/public_html/org/journal.org" 54 65 (org-attr nil)) #("/scpc:chris@192.168.1.200:/home/chris/public_html/org/mobileorg.org" 54 67 (org-attr nil)) "/scpc:chris@192.168.1.200:/home/chris/public_html/org/index.org")))
  '(org-startup-truncated nil)
  '(tool-bar-mode nil))
 (custom-set-faces
