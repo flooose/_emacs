f0ba5c28b95a03a7c596ae3bc3aa310ae6737244
christopher floess <skeptikos@gmail.com>
2014-03-14 09:49:37 +0100

    cleanup org-mode config

--- _config/scripts-available/org-mode.el
+++ _config/scripts-available/org-mode.el
@@ -31,20 +31,3 @@
                    (concat org-directory "/todo.org")
                    (concat org-directory "/shopping.org")
                    (concat org-directory "/vokab.org")))
-
-;;; org-todo-keywords has sane default
-;;; org-tag-alist still has to be set
-;
-
-
-;(setq org-agenda-files "/scpc:chris@192.168.1.200:public_html/org/") ;; when a directory, all org files are agenda files
-;;; org-mobile-files defaults to all agenda files
-;;; org-todo-keywords has sane default
-;;; org-tag-alist still has to be set
-;(setq org-mobile-directory "/scpc:chris@192.168.1.200:public_html/org/")
-;(setq org-default-notes-file (concat org-directory "/scpc:chris@192.168.1.200:public_html/org/notes.org"))
-;(define-key global-map "\C-cc" 'org-capture)
-;
-;(setq org-capture-templates
-;      '(("j" "Journal" entry (file+datetree "/scpc:chris@192.168.1.200:public_html/org/journal.org")
-;       "* %?\nEntered on %U\n  %i\n  %a")))
