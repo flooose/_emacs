4e1aa0641ddb169be811c3d84e8926d62d604adf
christopher floess <skeptikos@gmail.com>
2015-04-23 16:09:41 +0200

    shallow js3-mode indents

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,6 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function (quote browse-url-chromium))
+ '(custom-safe-themes
+   (quote
+    ("a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" "08851585c86abcf44bb1232bced2ae13bc9f6323aeda71adfa3791d6e7fea2b6" default)))
+ '(js3-consistent-level-indent-inner-bracket t)
  '(menu-bar-mode nil)
  '(org-agenda-files (quote ("/scp:192.168.1.200:/home/chris/public_html/org/mobileorg.org" #("/scpc:chris@192.168.1.200:/home/chris/public_html/org/journal.org" 54 65 (org-attr nil)) #("/scpc:chris@192.168.1.200:/home/chris/public_html/org/mobileorg.org" 54 67 (org-attr nil)) "/scpc:chris@192.168.1.200:/home/chris/public_html/org/index.org")))
  '(org-startup-truncated nil)
