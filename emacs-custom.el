17301487edcd7098107780c0b260a133557125f4
christopher floess <chris.floess@jjff.de>
2019-04-03 06:47:18 -0400

    Add packages and package customizations

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,12 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
  '(custom-safe-themes
    (quote
     ("2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" default)))
  '(package-selected-packages
    (quote
-    (flycheck-elm elm elm-mode gruvbox-theme projectile json-mode js2-mode coffee-mode))))
+    (highlight-indentation ace-window multiple-cursors haml-mode yaml-imenu yaml-mode expand-region ac-inf-ruby inf-ruby flycheck-elm elm elm-mode gruvbox-theme projectile json-mode js2-mode coffee-mode)))
+ '(safe-local-variable-values
+   (quote
+    ((flycheck-checker quote javascript-eslint)
+     (flycheck-checker . "javascript-eslint")
+     (mercury-merkur . "abc123")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
